27ec970f61ff6f43e538a2adfac2f62860becbf9
桥黎 <cql.ql@qq.com>
2018-05-10 11:04:09 +0800

    slider-one-load 组件完成

--- test/unit/components/slider-one-load.test.js
+++ test/unit/components/slider-one-load.test.js
@@ -1,12 +1,39 @@
 import Vue from 'vue'
 import SliderOneLoadDemoComponent from '@/components/slider-one-load/Demo.vue'
-xdescribe('slider-one-load', function () {
+import SliderOneLoad from '@/components/slider-one-load/Index.vue'
+describe('slider-one-load', function () {
   function getVm () {
+    // 此方式确保宽度不为 0
     const el = document.createElement('div')
     document.body.appendChild(el)
-    const SliderOneLoadDemo = Vue.extend(SliderOneLoadDemoComponent)
-    const vSliderOneLoadDemo = new SliderOneLoadDemo().$mount(el)
-    return vSliderOneLoadDemo.$refs.vSliderOneLoad
+
+    const vm = new Vue({
+      el,
+      template: `<SliderOneLoad ref="vSliderOneLoad" @load="onLoad" @setBlank="onSetBlank" :total="5">
+        <div v-for="(item, i) of items" class="item" :key="i" :slot="i" v-show="item.blank===false">
+          <p>page：{{item.page}}</p>{{item.content}}
+        </div>
+      </SliderOneLoad>`,
+      data () {
+        return {
+          items: [{
+            blank: true
+          }, {
+            blank: true
+          }, {
+            blank: true
+          }]
+        }
+      },
+      components: {
+        SliderOneLoad
+      },
+      methods: {
+        onLoad () {},
+        onSetBlank () {}
+      }
+    })
+    return vm.$refs.vSliderOneLoad
   }
 
   it('goPage 动画，去最后一页', function (done) {
@@ -24,10 +51,29 @@ xdescribe('slider-one-load', function () {
   })
 
   it('动画', function (done) {
-    getVm().animate(-1 * vm.width, done)
+    const vm = getVm()
+    vm.animate(-1 * vm.width, done)
   })
 
-  xit('触发当前页面加载', function () {
+  it('触发当前页面加载', function (done) {
+    const vm = getVm()
+    vm.$on('load', done)
+    vm.goPage(1, false)
+  })
 
+  it('移动，动画复原', function (done) {
+    const vm = getVm()
+
+    // 动画结束事件
+    function transitionend () {
+      done()
+    }
+
+    const eMove = vm.$el.children[0]
+    eMove.addEventListener('transitionend', transitionend)
+    eMove.addEventListener('webkitTransitionEnd', transitionend)
+
+    vm.move(-vm.width / 2)
+    vm.recovery()
   })
 })
