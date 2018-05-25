fbfa7b97272f6a351751d61248f9a209bb5c8f2e
桥黎 <cql.ql@qq.com>
2018-05-25 11:12:28 +0800

    demos-run 使用新方式

--- demos-run/src/nav-dev.js
+++ demos-run/src/nav-dev.js
@@ -1,20 +1,46 @@
 //
 
 // 批量导入路由
-let routes = []
-function importAll (r) {
-  r.keys().forEach(key => {
-    routes = routes.concat(r(key).default)
-  })
-}
+// let routes = []
+// function importAll (r) {
+//   r.keys().forEach(key => {
+//     routes = routes.concat(r(key).default)
+//   })
+// }
 
-importAll(require.context('./router', false, /\.js$/))
+// importAll(require.context('./router', false, /\.js$/))
 
-export default [
-  {
-    path: '/',
+// export default [
+//   {
+//     path: '/',
+//     name: 'navDev',
+//     meta: {title: '快捷导航-dev'},
+//     component: {
+//       render (h) {
+//         const h2Style = { padding: '20px 6px 0', 'font-size': '16px' }
+//         const pStyle = { padding: '4px 10px' }
+//         const aStyle = { color: 'blue', 'text-decoration': 'underline' }
+
+//         const links = []
+//         routes.forEach(route => {
+//           links.push(<p style={pStyle}><router-link to={route.path} style={aStyle}>{route.meta.title}</router-link></p>)
+//         })
+
+//         return (
+//           <div>
+//             <h2 style={h2Style}>demos</h2>
+//             {links}
+//           </div>
+//         )
+//       }
+//     }
+//   }
+// ]
+export default function (routes) {
+  routes.push({
+    path: '*',
     name: 'navDev',
-    meta: {title: '快捷导航-dev'},
+    meta: { title: '快捷导航-dev' },
     component: {
       render (h) {
         const h2Style = { padding: '20px 6px 0', 'font-size': '16px' }
@@ -23,7 +49,7 @@ export default [
 
         const links = []
         routes.forEach(route => {
-          links.push(<p style={pStyle}><router-link to={route.path} style={aStyle}>{route.meta.title}</router-link></p>)
+          if (route.path !== '*') links.push(<p style={pStyle}><router-link to={route.path} style={aStyle}>{route.path}</router-link></p>)
         })
 
         return (
@@ -34,5 +60,5 @@ export default [
         )
       }
     }
-  }
-]
+  })
+}
--- demos-run/src/router.js
+++ demos-run/src/router.js
@@ -1,31 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import navDev from './nav-dev'
 
-// 批量导入路由
 let routes = []
-function importAll (r) {
-  r.keys().forEach(key => {
-    routes = routes.concat(r(key).default)
-  })
-}
-
-// 指定只打包具体 router
-if (process.env.NODE_ENV === 'production') {
-  // 正式环境
-  importAll(require.context('./router', false, /(hello|hello2)\.js$/))
-} else {
-  // 开发环境
-
-  // 编译所有
-  importAll(require.context('./', true, /^\.\/router\/.+?\.js$|^\.\/nav-dev.js$/))
-
-  // 编译一个demo
-  // importAll(require.context('./router', false, /(forms)\.js$/))
-  // importAll(require.context('./router', false, /(confirm)\.js$/))
-  // routes[0].path = '/'
-}
 
 Vue.use(Router)
+
+function importAll (r) {
+  r.keys().forEach(key => {
+    routes.push({
+      path: key.match(/(\/[^/]+?)\/demo.vue/)[1],
+      component: r(key).default
+    })
+  })
+}
+// 编译所有 demo
+// importAll(require.context('@/', true, /^\.\/(components|modules)\/.+?\/demo\.vue$/))
+
+// 编译指定 demo
+importAll(require.context('@/', true, /^\.\/(components|modules)\/(base|buttons)\/demo\.vue$/))
+
+navDev(routes)
 
 export default new Router({
   routes: routes
