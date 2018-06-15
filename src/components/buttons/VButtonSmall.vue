9f34f9224916842ffc18f826f6c448be4c3863a0
桥黎 <cql.ql@qq.com>
2018-06-15 10:40:55 +0800

    新增

---
+++ src/components/buttons/VButtonSmall.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <VButton :s="s" @click="$emit('click')">
+    <slot/>
+  </VButton>
+</template>
+<script>
+import VButton from './VButton'
+export default {
+  props: {
+    s: String
+  },
+  components: {
+    VButton
+  }
+}
+</script>
+
+<style scoped>
+.b-btn {
+  width: auto;
+  height: 30px;
+  padding: 0px 16px;
+  font-size: 14px;
+  box-shadow: none;
+}
+</style>
