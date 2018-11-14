b6ab1a9d65e41b70ef4e4950a0ff12df9dde34cd
桥黎 <cql.ql@qq.com>
2018-11-14 18:06:56 +0800

    input-autosuggest

---
+++ projects/input-autosuggest_20181114/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <InputAutosuggest/>
+  </div>
+</template>
+
+<script>
+import InputAutosuggest from './input-autosuggest'
+export default {
+  components: {
+    InputAutosuggest
+  }
+}
+</script>
+
