b62876432289e2b5d7ec6e3b9818f4e9cf6e3f84
桥黎 <cql.ql@qq.com>
2018-11-29 18:08:11 +0800

    swipe 优化

---
+++ projects/swipe_20181129/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="box">
+    <p>hello</p>
+  </div>
+</template>
+
+<script>
+import swipex from './swipex.js'
+export default {
+  mounted () {
+    swipex({
+      elem: this.$el,
+      onMove () {}
+    })
+  }
+}
+</script>
+
+<style scoped>
+.box {
+  height: 600px;
+}
+.box>>>p {
+  background-color: #ddd;
+  margin: 0;
+}
+.box>>>p:nth-child(2n) {
+  background-color: #eee;
+}
+</style>
---
+++ projects/swipe_20181129/src/swipex.js
@@ -0,0 +1,114 @@
+import drag from '@/modules/corejs/dom-drag/drag'
+
+/**
+ * @param {function} onDown 可选 可在此处终止滑动开始
+ * @param {function} onStart 可选
+ * @param {function} onEnd 可选
+ * @param {function} onMove
+ * @param {function} onSwipeLeft 左滑，显示右边
+ * @param {function} onSwipeRight 右滑，显示左边
+ * @param {function} onSwipeNot 有移动，但没有切换
+ */
+
+export default function swipex ({ elem, onDown = () => {}, onStart = () => {}, onEnd = () => {}, onMove, onSwipeLeft, onSwipeRight, onSwipeNot }) {
+  let preX
+  let preY
+  let preTime
+  let xData = []
+  let isStart = false
+  let isCancel = false // 触发其他行为情况，比如滚动条
+
+  drag({
+    elem,
+    onDown (e) {
+      // 保证滑动动作只激活此一个实例
+      e.stopPropagation()
+      return onDown(e)
+    },
+    onStart: function (e) {
+      if (isCancel) return false
+      let touche = e.targetTouches ? e.targetTouches[0] : e
+      let { pageX, pageY } = touche
+      preX = pageX
+      preY = pageY
+      preTime = Date.now()
+    },
+    onMove: function (e) {
+      // 保证滑动动作只激活此一个实例
+      e.stopPropagation()
+
+      if (isCancel) return
+
+      let touche = e.targetTouches ? e.targetTouches[0] : e
+      let { pageX, pageY } = touche
+
+      let currX = pageX
+      let currY = pageY
+
+      let xlen = currX - preX
+      let ylen = currY - preY
+
+      if (isStart === false) {
+        // 手势相对于x轴 小于 45 度情况滑动才开始
+        // window.dlog(Math.atan(ylen / xlen), ylen / xlen, 'y:' + ylen, 'x:' + xlen)
+        // if (Math.abs(Math.atan(ylen / xlen)) < 1) {
+        if (Math.abs(ylen / xlen) < 1) {
+          isStart = true
+          onStart(e)
+        } else {
+          isCancel = true
+        }
+        // 调试，微调弧度值，直到最精确
+        // debugMsg(Math.abs(Math.atan(ylen / xlen)) + ' ' + (Math.abs(Math.atan(ylen / xlen)) < 1.2) + '  ' + isStart);
+      }
+
+      if (isStart) {
+        e.preventDefault()
+
+        let curTime = Date.now()
+        let timeLen = curTime - preTime
+        console.log(xlen.toFixed(2) * 1, timeLen)
+        // elem.innerHTML = `<p>move,${timeLen},${xlen.toFixed(2) * 1}</p>` + elem.innerHTML
+
+        xData.push([xlen.toFixed(2) * 1, timeLen])
+        onMove(xlen)
+        preX = currX
+        preY = currY
+        preTime = curTime
+      }
+    },
+    onEnd: function () {
+      if (isStart) {
+        xData.push([0, Date.now() - preTime, 'end'])
+        // console.log(JSON.stringify(xData))
+
+        let x = 0
+        let time = 0
+        for (let i = xData.length; i--;) {
+          let d = xData[i]
+          let t = d[1]
+          if (t > 100) {
+            break
+          }
+          x += d[0]
+          time += t
+        }
+        elem.innerHTML = `<p>${time},${JSON.stringify(xData)}</p>` + elem.innerHTML
+        let speed = x / time
+        let min = 0.15
+        if (speed > min) {
+          // onSwipeRight()
+        } else if (speed < -min) {
+          // onSwipeLeft()
+        } else {
+          // onSwipeNot()
+        }
+        xData = []
+        onEnd()
+        /* test */
+        // info.innerHTML = speed * 100
+      }
+      isCancel = isStart = false
+    }
+  })
+}
