7530ee32ec20810b5284ff19e79bcbe9385c8009
桥黎 <cql.ql@qq.com>
2018-12-05 18:14:47 +0800

    layout-edit 可视化布局

--- projects/layout-edit_20181204/src/App.vue
+++ projects/layout-edit_20181204/src/App.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="layout-edit">
     <!-- <div class="le-bar"></div> -->
-    <div class="le-r-set"></div>
+    <div class="le-r-set">
+      <dl>
+        <dt>选择控件</dt>
+        <dd>
+          <a href="">图片</a>
+          <a href="">幻灯片</a>
+          <a href="">文本</a>
+          <a href="">滚动文字</a>
+          <a href="">视频</a>
+        </dd>
+      </dl>
+      <dl>
+        <dt>控件</dt>
+        <dd>
+
+        </dd>
+      </dl>
+      <dl>
+        <dt>控件属性</dt>
+        <dd>
+
+        </dd>
+      </dl>
+    </div>
     <div class="le-edit-box"></div>
     <!-- <div class="le-main"></div> -->
   </div>
