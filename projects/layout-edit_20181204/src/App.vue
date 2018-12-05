2a94a68b34cb000de9381e12c57680be7e8fbdc8
桥黎 <cql.ql@qq.com>
2018-12-05 18:06:57 +0800

    layout-edit 可视化布局

--- projects/layout-edit_20181204/src/App.vue
+++ projects/layout-edit_20181204/src/App.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    hello
+  <div class="layout-edit">
+    <!-- <div class="le-bar"></div> -->
+    <div class="le-r-set"></div>
+    <div class="le-edit-box"></div>
+    <!-- <div class="le-main"></div> -->
   </div>
 </template>
+
+<style scoped>
+.layout-edit {
+
+}
+.le-bar {
+  height: 100px;
+  background-color: #ddd;
+}
+.le-main {
+
+}
+.le-r-set {
+  float: right;
+  width: 100px;
+  height: 100px;
+  background-color: #ddd;
+}
+.le-edit-box {
+  background-color: #aaa;
+  height: 100px;
+  margin-right: 100px;
+}
+
+</style>
