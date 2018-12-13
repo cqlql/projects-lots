d29dcdeb4c5f1699b2c6593d2396d0ae79b0aa3d
桥黎 <cql.ql@qq.com>
2018-12-13 15:01:24 +0800

    Create BaseTextareaFull.vue

---
+++ src/components/forms/BaseTextareaFull.vue
@@ -0,0 +1,43 @@
+<template>
+  <textarea class="tt" cols="30" rows="10" :value="value" @input="onInput"></textarea>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String
+  },
+  methods: {
+    onInput (e) {
+      this.$emit('inputValue', e.target.value)
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.tt {
+  background-color: #ffffff;
+  border: 1px solid #dcdcdc;
+  /* border: 1px solid #0ace82; */
+  /* border-radius: 1px; */
+  padding: 2px 12px;
+  border-radius: 5px;
+  box-sizing: border-box;
+  font-size: 14px;
+  color: #767575;
+  outline: none;
+
+  display: block;
+  width: 100%;
+  resize: none;
+}
+.tt:focus {
+  border-color: #0ace82;
+}
+.tt::placeholder {
+  color: #b7b6b6;
+}
+</style>
+
