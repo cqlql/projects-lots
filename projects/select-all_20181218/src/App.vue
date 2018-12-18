74578135b5c559a7e2a9cfa917e598253f8dfb86
陈桥黎 <cql.ql@qq.com>
2018-12-18 22:38:36 +0800

    Update App.vue

--- projects/select-all_20181218/src/App.vue
+++ projects/select-all_20181218/src/App.vue
@@ -10,6 +10,47 @@
 
 <script>
 import BaseCheckbox from '@/components/forms/BaseCheckbox'
+class SelectAll {
+  constructor () {
+    this.total = 0 // 可选择总数
+    this.selectNum = 0 // 当前选择数
+  }
+  // 重置
+  setSelectTotal (total) {
+    this.total = total
+    this.selectNum = 0
+  }
+  select (checked) {
+    if (checked) {
+      this.selectNum++
+    } else {
+      this.selectNum--
+    }
+    return this.getStates()
+  }
+  selectAll (checked) {
+    this.selectNum = this.total
+    return 1
+  }
+  // 0 没有选择 1 全选 2 有选择
+  getStates () {
+    if (this.selectNum === 0) {
+      return 0
+    }
+    if (this.selectNum === this.total) {
+      return 1
+    }
+    return 2
+  }
+  // // 是否全选
+  // isAll () {
+  //   return this.selectNum === this.total
+  // }
+  // // 没有任何选择
+  // isNothing () {
+  //   return this.selectNum === 0
+  // }
+}
 export default {
   components: {
     BaseCheckbox
@@ -20,16 +61,18 @@ export default {
     }
   },
   created () {
-    selectAll.setNotSelectNum(this.d.length)
+    this.selectAll = new SelectAll()
+    this.selectAll.setSelectTotal(this.d.length) // 设置可选择总数
   },
   methods: {
     change (checked) {
-      if (checked) {
-        selectAll.delNotSelect() // 减 1
-      }
+      this.selectAll.select(checked) // 根据参数决定 select notSelect 加减1
+      // if (checked) {
+      //   selectAll.delNotSelect() // 减 1
+      // }
     },
     onAllSelect (e, checked) {
-      selectAll.setNotSelectNum(0)
+      this.selectAll.selectAll(checked)
     }
   }
 }
