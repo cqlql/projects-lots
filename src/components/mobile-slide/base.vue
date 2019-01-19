a45f63b633a81a7132a76c6f19d95ac855667aef
陈桥黎 <cql.ql@qq.com>
2019-01-19 22:16:48 +0800

    更新

--- src/components/mobile-slide/base.vue
+++ src/components/mobile-slide/base.vue
@@ -1,9 +1,124 @@
-<template>
-  
-</template>
 
 <script>
+import autoprefix from '@/modules/corejs/dom-css/autoprefix'
 export default {
-  
+  props: {
+    imgs: {
+      type: Array,
+      default: () => { return [] }
+    }
+  },
+  data () {
+    return {
+      viewImgs: [],
+      currIndex: 0,
+      transformName: autoprefix('transform')
+    }
+  },
+  computed: {
+    count () {
+      return this.viewImgs.length
+    },
+    // 当前左边 index
+    currIndexLeft () {
+      let index = this.currIndex - 1
+      if (index < 0) {
+        return this.count - 1
+      }
+      return index
+    },
+    eItems () {
+      return this.$refs.eBox.children
+    },
+    eCurrItem () {
+      return this.eItems[this.currIndex]
+    }
+  },
+  watch: {
+    imgs () {
+      this.imgsSet()
+    }
+  },
+  created () {
+    this.imgsSet()
+  },
+  mounted () {
+    this.initShow()
+  },
+  methods: {
+    leftHide () {
+      // this.toCss(this.eCurrItem, -100)
+      this.animate(this.eCurrItem, -100)
+    },
+    rightShow () { },
+    swipeLeft () {
+      this.leftHide()
+      this.rightShow()
+    },
+    hide () {},
+
+    imgsSet () {
+      this.viewImgs = this.imgs
+    },
+    toCss (el, v) {
+      el.style[this.transformName] = `translateX(${v}%)`
+    },
+    animate (el, v) {
+      animate(el, this.$style.animated, {
+        [this.transformName]: `translateX(${v}%)`
+      })
+    },
+    initShow () {
+      this.toCss(this.eCurrItem, 0)
+    }
+  },
+  render () {
+    let { $style: s } = this
+    let sItem = s.item
+    let lis = []
+    this.viewImgs.forEach(url => {
+      lis.push(
+        <li class={sItem}>
+          <img src={url} />
+        </li>
+      )
+    })
+    return (
+      <ul ref="eBox" class={s.box}>{lis}</ul>
+    )
+  }
 }
 </script>
+
+<style module>
+.box {
+  /* height: 100%; */
+  width: 200px;
+  height: 100px;
+  background-color: #000;
+  overflow: hidden;
+  position: relative;
+}
+.item {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  text-align: center;
+  transform: translateX(-100%);
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate3d(-50%, -50%, 0);
+  }
+}
+.animated {
+  transition:transform 0.3s ease;
+}
+</style>
--- src/components/mobile-slide/demo.vue
+++ src/components/mobile-slide/demo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <MobileSlideBase />
+    <MobileSlideBase ref="vMobileSlideBase" :imgs="imgsBase" />
     <div>
-      <button>左滑动</button>
+      <button @click="$refs.vMobileSlideBase.swipeLeft()">左滑动</button>
       <button>右滑动</button>
     </div>
     <MobileSlide />
@@ -16,6 +16,17 @@ export default {
   components: {
     MobileSlideBase,
     MobileSlide
+  },
+  data () {
+    return {
+      imgsBase: [
+        'https://m.360buyimg.com/babel/jfs/t1/25652/29/5592/100061/5c3eeb60E6f839b18/c42da29b3661ec39.jpg',
+        'https://img1.360buyimg.com/pop/jfs/t1/23838/24/5600/92769/5c405bdfEf4c93379/554d6d8416870e23.jpg',
+        'https://img1.360buyimg.com/pop/jfs/t1/7107/16/13498/182625/5c41dd82Eab1ba239/46d813c63951032b.jpg',
+        'https://m.360buyimg.com/babel/jfs/t1/19482/24/5358/93845/5c3d3f9eEd3af48c9/cb2295923c4b3ab4.jpg'
+      ]
+
+    }
   }
 }
 </script>
