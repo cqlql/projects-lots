cba3f0470b52d51736dc641f0df6b26b86aba0be
陈桥黎 <cql.ql@qq.com>
2019-02-17 19:34:45 +0800

    select-search

--- projects/list-select-hostkey_20190217/src/demo.vue
+++ projects/list-select-hostkey_20190217/src/demo.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="box">
-    <ListSelect />
+    <ListSelect ref="vListSelect" />
+    <div>
+      <button @click="highlightPrev">高亮上一个</button>
+      <button @click="highlightNext">高亮下一个</button>
+      <button @click="highlight2">高亮第二个</button>
+      <button @click="highlightLast2">高亮倒数第二个</button>
+    </div>
+    <!-- <input type="text" @keyup="keyup"> -->
   </div>
 </template>
 
@@ -9,6 +16,27 @@ import ListSelect from './ListSelect.vue'
 export default {
   components: {
     ListSelect
+  },
+  methods: {
+    // keyup (e) {
+    //   console.log(e)
+    // },
+    highlightPrev () {
+      this.$refs.vListSelect.highlightPrev()
+    },
+    highlightNext () {
+      this.$refs.vListSelect.highlightNext()
+    },
+    highlight2 () {
+      this.$refs.vListSelect.highlight(
+        1
+      )
+    },
+    highlightLast2 () {
+      this.$refs.vListSelect.highlight(
+        this.$refs.vListSelect.eItems.length - 2
+      )
+    }
   }
 }
 </script>
---
+++ projects/select-search_20190217/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <span :class="$style.select">
+      <span :class="$style.container">asdsa</span>
+      <span :class="$style.arrow"><b /></span>
+    </span>
+  </div>
+</template>
+
+<style module>
+.select {
+  background-color: #fff;
+  border: 1px solid #aaa;
+  border-radius: 4px;
+
+  box-sizing: border-box;
+  cursor: pointer;
+  display: block;
+  height: 28px;
+  user-select: none;
+  position: relative;
+}
+.container {
+  color: #444;
+  line-height: 28px;
+  display: block;
+  padding-left: 8px;
+  padding-right: 20px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  box-sizing: border-box;
+}
+.arrow {
+  height: 26px;
+  position: absolute;
+  top: 1px;
+  right: 1px;
+  width: 20px;
+}
+.arrow b {
+  border-color: #888 transparent transparent transparent;
+  border-style: solid;
+  border-width: 5px 4px 0 4px;
+  height: 0;
+  left: 50%;
+  margin-left: -4px;
+  margin-top: -2px;
+  position: absolute;
+  top: 50%;
+  width: 0;
+}
+.open b {
+  border-color: transparent transparent #888 transparent;
+  border-width: 0 4px 5px 4px;
+}
+</style>
