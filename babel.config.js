285e9a81db87d0125898c6ff69cc9a8f5d67ac86
陈桥黎 <cql.ql@qq.com>
2019-02-17 20:54:31 +0800

    按需 polyfill

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "presets": [
     ["@babel/preset-env", {
       "modules": false,
+      "useBuiltIns": "usage",
       "targets": {
         "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
       }
