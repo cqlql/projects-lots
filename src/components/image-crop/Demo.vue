4e8ec32196cfb230f154c87326ba299eb4a23834
陈桥黎 <cql.ql@qq.com>
2019-03-16 10:39:47 +0800

    图片裁剪更新

--- src/components/image-crop/Demo.vue
+++ src/components/image-crop/Demo.vue
@@ -1,9 +1,145 @@
 <template>
   <div>
-    <div class="slecteBox">
-      <div class="imgBox">
-        <img class="img" src="">
+    <div :class="$style.selectArea">
+      <div
+        :class="$style.slecteBox"
+        :style="{
+          left: slecteBoxStyle.x + 'px',
+          top: slecteBoxStyle.y + 'px',
+          width: slecteBoxStyle.w + 'px',
+          height: slecteBoxStyle.h + 'px',
+        }"
+      >
+        <div
+          ref="eImgBox"
+          :class="$style.imgBox"
+          :style="{
+            width: imgBoxStyle.w + 'px',
+            height: imgBoxStyle.h + 'px',
+            [transformName]: `translate(${imgBoxStyle.x}px,${imgBoxStyle.y}px) scale(${imgBoxStyle.scale}) rotate(${imgBoxStyle.rotate}deg)`
+          }"
+        >
+          <img
+            :src="url"
+            :style="{
+              width: imgStyle.w + 'px',
+              height: imgStyle.h + 'px',
+              [transformName]: `translate(${imgStyle.x}px,${imgStyle.y}px) rotate(${imgStyle.rotate}deg)`
+            }"
+          >
+        </div>
       </div>
     </div>
-  </div>  
+    <div :class="$style.bottomBar">
+      <button>取消</button>
+      <button>还原</button>
+      <button>完成</button>
+    </div>
+  </div>
 </template>
+
+<script>
+import imgSize from '@/modules/corejs/dom/img-size-complete.js'
+import center from '@/modules/corejs/center/center.js'
+import autoprefix from '@/modules/corejs/dom-css/autoprefix'
+import ZoomTouch from '@/modules/zoom-touch2/zoom-touch.js'
+export default {
+  data () {
+    return {
+      aspectRatio: 16 / 9, // 选择框 宽/高 比
+      selectAreaStyleWidth: 500,
+      selectAreaStyleHeight: 500,
+      slecteBoxStyle: {
+        h: 0,
+        w: 0,
+        x: 0,
+        y: 0
+      },
+      url: 'https://img13.360buyimg.com/n1/s350x449_jfs/t18994/37/1125997630/238618/15b9a7d/5abc8fccN013d0d0a.jpg!cc_350x449.jpg',
+      imgBoxStyle: {
+        w: 300,
+        h: 100,
+        x: 0,
+        y: 0,
+        scale: 1,
+        rotate: 0
+      },
+      imgStyle: {
+        w: 300,
+        h: 100,
+        x: 0,
+        y: 0,
+        rotate: 0
+      },
+      transformName: autoprefix('transform'),
+    }
+  },
+  created () {
+    this.sizeUpdate()
+    this.slecteBoxStyleUpdate()
+    this.imgSizeUpdate()
+  },
+  mounted () {
+    let { imgBoxStyle } = this
+    let zoomTouch = new ZoomTouch(this.$refs.eImgBox)
+    zoomTouch.css = function () {
+      let { x, y, scale } = this
+      imgBoxStyle.x = x
+      imgBoxStyle.y = y
+      imgBoxStyle.scale = scale
+    }
+  },
+  methods: {
+    // 窗口尺寸更新
+    sizeUpdate () {
+      let slecteBoxBorderWidth = 4 // 选择框边框宽
+      let margin = 20 // selectArea 间距
+      this.selectAreaStyleWidth = window.innerWidth - slecteBoxBorderWidth - margin
+      this.selectAreaStyleHeight = window.innerHeight - 100 - slecteBoxBorderWidth - margin
+    },
+    // 选择框尺寸坐标更新
+    slecteBoxStyleUpdate () {
+      let { slecteBoxStyle, selectAreaStyleWidth, selectAreaStyleHeight } = this
+      let { x, y, w, h } = center(selectAreaStyleWidth, selectAreaStyleHeight, selectAreaStyleWidth, selectAreaStyleWidth / this.aspectRatio)
+      slecteBoxStyle.x = x
+      slecteBoxStyle.y = y
+      slecteBoxStyle.w = w
+      slecteBoxStyle.h = h
+    },
+    // 图片尺寸更新
+    async imgSizeUpdate () {
+      let { imgBoxStyle, imgStyle } = this
+      let img
+      try {
+        img = await imgSize(this.url)
+        imgStyle.w = imgBoxStyle.w = img.width
+        imgStyle.h = imgBoxStyle.h = img.height
+        console.log(imgStyle)
+      } catch (err) {
+        console.error('图片尺寸获取失败', err)
+      }
+    }
+  }
+}
+</script>
+
+<style module>
+.selectArea {
+  margin: 10px;
+}
+.slecteBox {
+  border: 2px solid #333;
+  position: relative;
+}
+.imgBox {
+  /* transform-origin: 0 0; */
+}
+.bottomBar {
+  width: 100%;
+  height: 100px;
+  background-color: #ddd;
+  position: fixed;
+  bottom: 0;
+}
+
+</style>
---
+++ src/components/image-crop/zoom-touch.js
@@ -0,0 +1,66 @@
+import DragZoom from '@/modules/zoom-touch2/drag-zoom.js'
+import autoPrefix from '@/modules/corejs/dom-css/autoprefix'
+/**
+ * @param isInit 初始更新部分参数，更新元素位置参数
+ */
+export default class ZoomTouch extends DragZoom {
+  constructor (params) {
+    let { elem, isInit = true } = params
+    if (!elem) elem = params
+    super(elem)
+
+    this.elem = elem
+    this.elClientRect = { left: 0, top: 0, width: 0, height: 0 }
+    this.transform = autoPrefix('transform')
+    this.transformOriginX = 0
+    this.transformOriginY = 0
+    this.x = 0
+    this.y = 0
+    this.prex = 0
+    this.prey = 0
+    this.startx = 0
+    this.starty = 0
+    this.scale = 1
+    this.preScale = 1
+
+    // elem.style[autoPrefix('transform-origin')] = '0 0'
+
+    // 是否初始更新，
+    if (isInit) {
+      this.update()
+    }
+  }
+  // 松开情况，只要还有手指在，就会触发
+  onStart (clientX, clientY) {
+    let x = this.prex = this.x
+    let y = this.prey = this.y
+    this.preScale = this.scale
+    this.startx = clientX
+    this.starty = clientY
+
+    let { elClientRect, scale } = this
+    this.transformOriginX = (clientX - elClientRect.left - x) / (elClientRect.width * scale)
+    this.transformOriginY = (clientY - elClientRect.top - y) / (elClientRect.height * scale)
+  }
+  onMove (clientX, clientY, scale) {
+    let { preScale, elClientRect } = this
+
+    this.scale = scale * preScale
+
+    // 放大后偏移的 xy
+    let toScale = (scale - 1) * preScale
+    let sx = elClientRect.width * toScale * this.transformOriginX
+    let sy = elClientRect.height * toScale * this.transformOriginY
+
+    this.x = this.prex + clientX - this.startx - sx
+    this.y = this.prey + clientY - this.starty - sy
+    this.css()
+  }
+  update () {
+    // 注意：getBoundingClientRect 不会忽略 transform，而这里需要得到元素忽略 transform 的原始高宽坐标，待改正
+    this.elClientRect = this.elem.getBoundingClientRect()
+  }
+  css () {
+    this.elem.style[this.transform] = `translate(${this.x}px,${this.y}px) scale(${this.scale})`
+  }
+}
