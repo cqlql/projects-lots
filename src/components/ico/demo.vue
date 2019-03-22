8e73d01711d82e7fd5d2a3433c4941324cec2c84
桥黎 <cql.ql@qq.com>
2019-03-22 18:15:46 +0800

    新图标

---
+++ src/components/ico/demo.vue
@@ -0,0 +1,27 @@
+<script>
+import IcoTick from './IcoTick.vue'
+import IcoDetail from '@/components/morality-manager/@jony/comm/ico/IcoDetail.vue'
+export default {
+  render () {
+    let { $style } = this
+    return (
+      <div>
+        <IcoTick class={$style.iocTick} />
+        <IcoDetail class={$style.iocDetail} />
+      </div>
+    )
+  }
+}
+</script>
+
+<style module>
+.iocTick {
+  width: 20px;
+  height: 20px;
+}
+.iocDetail {
+  width: 20px;
+  height: 20px;
+  color: #b084f9;
+}
+</style>
