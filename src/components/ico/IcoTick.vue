ebc1a719596dd0eefd2e8a445ef450bb286a2f33
陈桥黎 <cql.ql@qq.com>
2019-04-11 08:33:47 +0800

    ico 更新

--- src/components/ico/IcoTick.vue
+++ src/components/ico/IcoTick.vue
@@ -3,11 +3,11 @@
 </template>
 
 <script>
-let icoAdd = false
+let exist = false
 export default {
   created () {
-    if (icoAdd) return
-    icoAdd = true
+    if (exist) return
+    exist = true
     let eTemp = document.createElement('div')
     eTemp.innerHTML = `<svg style="position: absolute; width: 0px; height: 0px; overflow: hidden;"><symbol id="${this.$style.ico}" viewBox="0 0 1024 1024"><path d="M343.163095 870.4l-319.015385-307.2c-31.507692-31.507692-31.507692-78.769231-3.938462-110.276923l0 0c31.507692-31.507692 78.769231-31.507692 110.276923-3.938462l259.938462 252.061538L886.670788 157.538462c27.569231-31.507692 78.769231-35.446154 110.276923-3.938462l0 0c31.507692 27.569231 35.446154 78.769231 3.938462 110.276923L457.37848 866.461538C425.870788 901.907692 374.670788 901.907692 343.163095 870.4z" fill="currentColor"></path></symbol></svg>`
     document.body.appendChild(eTemp.children[0])
