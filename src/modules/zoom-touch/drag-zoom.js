9f2659399724b8b12d72634ee341603b2ebda702
桥黎 <cql.ql@qq.com>
2019-04-12 15:11:54 +0800

    zoom-touch 整理

--- src/modules/zoom-touch/drag-zoom.js
+++ src/modules/zoom-touch/drag-zoom.js
@@ -1,72 +1,76 @@
-/**
- * DragZoom
- * */
-
-import drag from '../corejs/dom-drag/drag-mobile'
-
-export default function ({elem, onStart, onMove}) {
-  let zStartLen
-
-  function singleStart ({pageX, pageY}) {
-    onStart(pageX, pageY)
+import drag from '../corejs/drag/drag-mobile'
+export default class ZoomTouch {
+  constructor (elem) {
+    this.zStartLen = 0
+    this.dragZoom(elem)
   }
-
-  function zoomStart ({0: t1, 1: t2}) {
-    let {pageX: x1, pageY: y1} = t1
-    let {pageX: x2, pageY: y2} = t2
+  onDown () {}
+  onStart (pageX, pageY) {}
+  onMove (pageX, pageY, scale) {}
+  onEnd () {}
+  dragZoom (elem) {
+    this.destroy = drag({
+      elem,
+      onDown: e => this.onDown(e),
+      onStart: e => {
+        let { touches } = e
+        let { length } = touches
+        if (length > 1) {
+          this.zoomStart(touches)
+        } else {
+          this.singleStart(touches[0])
+        }
+      },
+      onMove: e => {
+        e.preventDefault()
+        let { touches } = e
+        let { length } = touches
+        if (length > 1) {
+          this.zoomMove(touches)
+        } else {
+          this.singleMove(touches[0])
+        }
+      },
+      onEnd: e => this.onEnd()
+    })
+  }
+  singleStart ({ pageX, pageY }) {
+    this.onStart(pageX, pageY)
+  }
+  zoomStart ({ 0: t1, 1: t2 }) {
+    let { pageX: x1, pageY: y1 } = t1
+    let { pageX: x2, pageY: y2 } = t2
 
     let xlen = x2 - x1
     let ylen = y2 - y1
 
-    zStartLen = Math.sqrt(Math.pow(xlen, 2) + Math.pow(ylen, 2))
+    this.zStartLen = Math.sqrt(Math.pow(xlen, 2) + Math.pow(ylen, 2))
 
     // 中心点
     let cx = x1 + xlen / 2
     let cy = y1 + ylen / 2
-    onStart(cx, cy)
+    this.onStart(cx, cy)
   }
 
-  function singleMove (touche) {
-    let {pageX, pageY} = touche
-    onMove(pageX, pageY, 1)
+  singleMove (touche) {
+    let { pageX, pageY } = touche
+    this.onMove(pageX, pageY, 1)
   }
-
-  function zoomMove ({0: t1, 1: t2}) {
-    let {pageX: x1, pageY: y1} = t1
-    let {pageX: x2, pageY: y2} = t2
+  zoomMove ({ 0: t1, 1: t2 }) {
+    let { pageX: x1, pageY: y1 } = t1
+    let { pageX: x2, pageY: y2 } = t2
     let xlen = x2 - x1
     let ylen = y2 - y1
 
     let zLen = Math.sqrt(Math.pow(xlen, 2) + Math.pow(ylen, 2))
-    let scale = zLen / zStartLen
+    let scale = zLen / this.zStartLen
 
     // 中心点
     let cx = x1 + xlen / 2
     let cy = y1 + ylen / 2
 
-    onMove(cx, cy, scale)
+    this.onMove(cx, cy, scale)
   }
-
-  drag({
-    elem,
-    onStart: (e) => {
-      let touches = e.targetTouches
-      let {length} = touches
-      if (length > 1) {
-        zoomStart(touches)
-      } else {
-        singleStart(touches[0])
-      }
-    },
-    onMove: (e) => {
-      e.preventDefault()
-      let touches = e.targetTouches
-      let {length} = touches
-      if (length > 1) {
-        zoomMove(touches)
-      } else {
-        singleMove(touches[0])
-      }
-    }
-  })
+  // 销毁，目前只用来解除相关触摸事件
+  destroy () {}
 }
