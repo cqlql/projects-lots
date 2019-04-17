96a1e9829815031dbc969639331e4f04c55f0d5f
桥黎 <cql.ql@qq.com>
2019-04-17 16:07:23 +0800

    Update timer.js

--- src/modules/corejs/time/timer.js
+++ src/modules/corejs/time/timer.js
@@ -3,18 +3,19 @@
  * */
 export default class Timer {
   /**
-   * @param {Function} tryStart 只有满足条件才能开始，默认 true
+   * @param {Function} allowStart 必须满足此条件才能开始，默认 true
    */
-  constructor ({ callBack, time, tryStart = () => true }) {
+  constructor ({ callBack, time, allowStart = () => true }) {
     this.time = time
     this.callBack = callBack
-    this.tryStart = tryStart
+    this.allowStart = allowStart
+    this.stopId = null
   }
 
   // 开始/重新开始
   start () {
     this.stop()
-    if (!this.tryStart()) return
+    if (!this.allowStart()) return
 
     let { time, callBack } = this
 
@@ -28,7 +29,14 @@ export default class Timer {
     loop()
   }
 
+  // 开始，只有停止情况才开始
+  tryStart () {
+    if (this.stopId) return
+    this.start()
+  }
+
   stop () {
     clearTimeout(this.stopId)
+    this.stopId = null
   }
 }
