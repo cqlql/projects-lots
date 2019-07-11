b4288b7b0207f636954cc00d2718f4699082d616
陈桥黎 <cql.ql@qq.com>
2019-07-11 11:30:26 +0800

    loading toast 声明文件

--- src/components/loading/plugin.d.ts
+++ src/components/loading/plugin.d.ts
@@ -1,4 +1,10 @@
 import Vue, { PluginFunction } from 'vue'
 
+declare module 'vue/types/vue' {
+  interface Vue {
+    $loading: any
+  }
+}
+
 declare const LoadingPlugin: PluginFunction<Vue>
 export default LoadingPlugin
--- src/components/toast/plugin.d.ts
+++ src/components/toast/plugin.d.ts
@@ -1,4 +1,10 @@
 import Vue, { PluginFunction } from 'vue'
 
+declare module 'vue/types/vue' {
+  interface Vue {
+    $toast: any
+  }
+}
+
 declare const ToastPlugin: PluginFunction<Vue>
 export default ToastPlugin
