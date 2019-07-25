257e27304ff941a6800d3fc7366527080212d69d
陈桥黎 <cql.ql@qq.com>
2019-07-25 18:32:11 +0800

    _demos 测试

--- projects/_demos/src/App.vue
+++ projects/_demos/src/App.vue
@@ -7,3 +7,12 @@
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Prop, Component } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+}
+</script>
---
+++ projects/_demos/src/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{name}}  
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+import { Prop, Component } from 'vue-property-decorator'
+
+@Component
+export default class Test extends Vue {
+  name:string|undefined
+}
+</script>
