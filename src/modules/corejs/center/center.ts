affa2162473b5dda3c9610857b9b4b3fa0658cb7
陈桥黎 <cql.ql@qq.com>
2019-08-22 23:38:39 +0800

    Create center.ts

---
+++ src/modules/corejs/center/center.ts
@@ -0,0 +1,26 @@
+// 居中，元素完全展示
+export default function center (boxW: number, boxH: number, elW: number, elH: number) {
+  let boxRatio = boxW / boxH
+  let elRatio = elW / elH
+  let x
+  let y
+  let w
+  let h
+  if (boxRatio > elRatio) { // 宽度大于元素宽
+    h = boxH
+    w = h * elRatio
+    x = (boxW - w) / 2
+    y = 0
+  } else {
+    w = boxW
+    h = w / elRatio
+    x = 0
+    y = (boxH - h) / 2
+  }
+  return {
+    x,
+    y,
+    w,
+    h
+  }
+}
