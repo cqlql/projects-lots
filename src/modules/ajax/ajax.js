fcea96ae7c59bf18cd6a8aa093034861e2bbe534
陈桥黎 <cql.ql@qq.com>
2020-05-21 12:13:29 +0800

    Update ajax.js

--- src/modules/ajax/ajax.js
+++ src/modules/ajax/ajax.js
@@ -1,24 +1,23 @@
 ﻿import querystring from '@/modules/corejs/url/querystring.js'
 function ajax (config) {
   return new Promise((resolve, reject) => {
-    let { url, method, params, responseType = 'json' } = config
+    let { url, method = 'GET', params, responseType = 'json' } = config
     method = method.toUpperCase()
     const xhr = new XMLHttpRequest()
     function readystatechange () {
-      if (xhr.readyState === 4) {
-      // if (xhr.readyState === xhr.DONE) {
-        if (xhr.status === 200) {
-          const response = xhr.response
+      if (xhr.readyState !== 4) return
+      // if (xhr.readyState === xhr.DONE) return
+      if (xhr.status === 200) {
+        const response = xhr.response
 
-          // android 4.4以下，ajax不会根据后台响应要求自动转换 json为对象。所以只能手动转换
-          if (responseType === 'json' && typeof response === 'string') {
-            resolve(JSON.parse(response))
-          } else {
-            resolve(response)
-          }
+        // android 4.4以下，ajax不会根据后台响应要求自动转换 json为对象。所以只能手动转换
+        if (responseType === 'json' && typeof response === 'string') {
+          resolve(JSON.parse(response))
         } else {
-          reject(new Error(method + ' ' + xhr.responseURL + ' ' + xhr.statusText))
+          resolve(response)
         }
+      } else {
+        reject(new Error(method + ' ' + xhr.responseURL + ' ' + xhr.statusText))
       }
     }
 
@@ -36,7 +35,7 @@ function ajax (config) {
     xhr.open(method, url)
     switch (method) {
       case 'POST':
-        post(xhr, config)
+        postSend(xhr, config)
         break
       case 'GET':
         xhr.send(null)
@@ -44,7 +43,7 @@ function ajax (config) {
     }
   })
 }
-function post (xhr, { data }) {
+function postSend (xhr, { data }) {
   if (data) {
     // 发送的数据类型
     if (typeof data === 'string') {
@@ -55,6 +54,18 @@ function post (xhr, { data }) {
       xhr.send(JSON.stringify(data))
     }
   }
+}
+
+ajax.post = function (url, data, config = {}) {
+  config.method = 'POST'
+  config.url = url
+  config.data = data
+  return ajax(config)
+}
+ajax.get = function (url, config = {}) {
+  config.method = 'GET'
+  config.url = url
+  return ajax(config)
 }
 
 export default ajax
