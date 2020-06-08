b64f44db9db24da8f47db0bbf962e3f6b9f85533
陈桥黎 <cql.ql@qq.com>
2020-06-08 23:12:50 +0800

    Update css.js

--- src/utils/corejs/css/css.js
+++ src/utils/corejs/css/css.js
@@ -2,9 +2,8 @@
 import autoprefix from './autoprefix'
 export default function (el, values = {}) {
   const { style } = el
-  for (const k in values) {
-    if (Object.prototype.hasOwnProperty.call(values, k)) {
-      style[autoprefix(k)] = values[k]
-    }
+  for (const k of values) {
+    // if (Object.prototype.hasOwnProperty.call(values, k))
+    style[autoprefix(k)] = values[k]
   }
 }
