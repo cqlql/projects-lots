af21ce6c6da8a4acc36b9c8c5aff22d3918dbaa8
陈桥黎 <cql.ql@qq.com>
2020-11-10 23:05:26 +0800

    Update mock-server

--- mock-server/router.js
+++ mock-server/router.js
@@ -4,17 +4,18 @@ const dataUrls = require('./data-urls')
 const router = express.Router()
 
 function getJson (req, res) {
-  let fileName = dataUrls[req.url]
+  const fileName = dataUrls[req.url]
   if (fileName === undefined) {
-    return res.sendStatus(404)
+    res.sendStatus(404)
+  } else {
+    fs.readFile(`${__dirname}/data/${dataUrls[req.url]}.json`, 'utf8', function (err, data) {
+      if (err) {
+        res.send(err)
+      } else {
+        res.send(data)
+      }
+    })
   }
-  fs.readFile(`${__dirname}/data/${dataUrls[req.url]}.json`, 'utf8', function (err, data) {
-    if (err) {
-      return res.send(err)
-    } else {
-      res.send(data)
-    }
-  })
 }
 
 router.post('/*', getJson)
