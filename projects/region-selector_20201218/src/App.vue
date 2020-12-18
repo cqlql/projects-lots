0e926479a7baac600dd9f478f7a91130d6356847
陈桥黎 <cql.ql@qq.com>
2020-12-18 16:56:39 +0800

    region-selector first commit

---
+++ projects/region-selector_20201218/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="s">
+    <div class="ipt"></div>
+    <RegionSelector />
+  </div>
+</template>
+
+<script>
+import RegionSelector from './RegionSelector'
+export default {
+  components: {
+    RegionSelector
+  }
+}
+</script>
+
+
+<style scoped>
+.s {
+  color: red;
+  font-size: 20px;
+}
+.ipt {
+  height: 38px;
+  width: 300px;
+  border: 1px solid #ddd;
+}
+</style>
---
+++ projects/region-selector_20201218/src/RegionSelector.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="region-selector">
+    <div class="header"></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      regionData: {
+        provinces: [
+          {
+            code: "120000",
+            value: "天津市"
+          }
+        ]
+      }
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.region-selector {
+  border: 1px solid #ddd;
+}
+</style>
