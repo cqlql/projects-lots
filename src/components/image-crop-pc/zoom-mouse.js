bc804660c037921370df232bdbc86757faa0a7f5
陈桥黎 <cql.ql@qq.com>
2021-01-04 20:39:35 +0800

    image-crop-pc 初步完成

--- src/components/image-crop-pc/zoom-mouse.js
+++ src/components/image-crop-pc/zoom-mouse.js
@@ -1,6 +1,8 @@
 import drag from '@/utils/corejs/drag/drag-pc.js'
 import mouseWheel from '@/utils/corejs/dom/mouse-wheel'
 import elPageXY from '@/utils/corejs/xy/el-page-xy'
+import ExcuDelay from '@/utils/corejs/excu/excu-delay'
+
 export default class ZoomMouse {
   constructor ({ elem }) {
     this.elem = elem
@@ -14,66 +16,46 @@ export default class ZoomMouse {
   }
 
   init () {
-    this.update()
+    this.updateBoxWH()
     const elem = this.elem
-    console.log(elem)
     let sx // 起始
     let sy
     let px // 上一次
     let py
     let preScale
-    const whr = this.w / this.h
-    const zoomScale = 0.1
-    const zoomWidthVal = 50
-    const zoomHeightVal = zoomWidthVal / whr
-    mouseWheel(elem, ({ deltaY, pageX, pageY }) => {
-      let { w, h, x, y, scale, elClientRect } = this
-      // const { x: elXOffset, y: elYOffset } = elPageXY(elem)
-      
-      // 鼠标指针位置比例
-      // const rx = (pageX - elXOffset) / w
-      // const ry = (pageY - elYOffset) / h
-      // console.log(pageX - elXOffset, pageY - elYOffset)
+    // const whr = this.w / this.h
+    const zoomScale = 0.1 // 每次缩放要加减的值
+    // const zoomWidthVal = 50
+    var excuDelay = new ExcuDelay()
+    this.unbindMouseWheel = mouseWheel(elem, ({ deltaY, pageX, pageY }) => {
+      this.updateBoxXY()
+      const { x, y, scale, elClientRect } = this
 
       preScale = scale
 
       this.transformOriginX = (pageX - elClientRect.left - x) / (elClientRect.width * scale)
       this.transformOriginY = (pageY - elClientRect.top - y) / (elClientRect.height * scale)
-
+      let zoomScaleVal
       if (deltaY < 0) {
         // 放大
-        this.scale = scale = scale * (1 + zoomScale)
-
-        // 放大后偏移的 xy
-        const toScale = (scale - 1) * preScale
-        const sx = elClientRect.width * toScale * this.transformOriginX
-        const sy = elClientRect.height * toScale * this.transformOriginY
-        
-        this.x = this.x - sx
-        this.y = this.y - sy
-
-        // w += zoomWidthVal
-        // h += zoomHeightVal
-        // x = x - zoomWidthVal * rx
-        // y = y - zoomHeightVal * ry
-        // scale = this.w / w
+        zoomScaleVal = zoomScale
       } else {
         // 缩小
-        this.scale = this.scale * (1 - zoomScale)
-        // w -= zoomWidthVal
-        // h -= zoomHeightVal
-        // x += zoomWidthVal * rx
-        // y += zoomHeightVal * ry
-        // scale = this.w / w
+        zoomScaleVal = -zoomScale
       }
-      // _this.x = x
-      // _this.y = y
-      // _this.w = w
-      // _this.h = h
-      // _this.scale = scale
+      this.scale = this.scale * (1 + zoomScaleVal)
+
+      // 放大后偏移的 xy
+      const toScale = zoomScaleVal * preScale
+      const sx = elClientRect.width * toScale * this.transformOriginX
+      const sy = elClientRect.height * toScale * this.transformOriginY
+
+      this.x = this.x - sx
+      this.y = this.y - sy
       this.css()
+      excuDelay.excu(() => this.onEnd(), 100)
     })
-    drag({
+    this.unbindDrag = drag({
       elem,
       onDown: e => this.onDown(e),
       onStart: e => {
@@ -96,21 +78,27 @@ export default class ZoomMouse {
     })
   }
 
-  update () {
-    // 注意：getBoundingClientRect 不会忽略 transform，而这里需要得到元素忽略 transform 的原始高宽坐标，待改正
-    // this.elClientRect = this.elem.getBoundingClientRect()
+  unbindMouseWheel () {}
+  unbindDrag () {}
 
-    // 改正后
+  updateBoxWH () {
     const { elClientRect, elem } = this
-    const { x, y } = elPageXY(elem)
-    elClientRect.left = x
-    elClientRect.top = y
     elClientRect.width = elem.offsetWidth
     elClientRect.height = elem.offsetHeight
   }
 
+  updateBoxXY () {
+    const { elClientRect, elem } = this
+    const { x, y } = elPageXY(elem)
+    elClientRect.left = x
+    elClientRect.top = y
+  }
+
   // 解除可能会重复注册的事件
-  destroy () {}
+  destroy () {
+    this.unbindMouseWheel()
+    this.unbindDrag()
+  }
 
   css () {}
   onDown () {}
