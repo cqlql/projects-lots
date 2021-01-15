4d09a750cc1eae78d80d51cf44838756c61cd376
陈桥黎 <cql.ql@qq.com>
2021-01-15 22:53:13 +0800

    Update request-common.js

--- src/utils/request/request-common.js
+++ src/utils/request/request-common.js
@@ -1,12 +1,16 @@
-export class RequestCommon {
-  constructor (dataHandle, axios) {
+/**
+ * 可通过实例重写覆盖或者直接 constructor 中带参覆盖 showLoading hideLoading messageError，
+*/
+
+export default class RequestCommon {
+  constructor ({ dataHandle, axios }) {
     this.axios = axios
     if (dataHandle) {
       this.dataHandle = dataHandle
     } else {
       this.dataHandle = function (data) {
-        if (data.status === 200) {
-          return data.result
+        if (data.code === 0) {
+          return data.data
         }
         return new Error(data.message)
       }
@@ -14,28 +18,27 @@ export class RequestCommon {
   }
 
   request (config) {
-    const { loading } = this
-    let { hasLoading = true } = config
-    hasLoading = hasLoading && (loading !== undefined)
-    if (hasLoading) loading.show()
+    config.timeout = 60000
+
+    const { hasLoading = true } = config
+    if (hasLoading) this.showLoading()
 
     // 开发环境专用配置
-    if (process.env.NODE_ENV !== 'production') {
-      config.baseURL = require('@/dev.config.js').default.baseURL
-    }
+    // if (process.env.NODE_ENV !== 'production') {
+    //   config.baseURL = require('@/dev.config.js').default.baseURL
+    // }
 
-    config.timeout = 60000
-    return this.axios(config).then(({ data }) => {
+    return this.axios(config).then(data => {
       const result = this.dataHandle(data)
       // 失败情况
       if (result instanceof Error) {
         return Promise.reject(result)
       }
       // 成功情况
-      if (hasLoading) loading.hide()
+      if (hasLoading) this.hideLoading()
       return result
     }).catch(e => {
-      if (hasLoading) loading.hide()
+      if (hasLoading) this.hideLoading()
       this.messageError(e.message)
       return Promise.reject(e)
     })
@@ -54,18 +57,29 @@ export class RequestCommon {
     return this.request(config)
   }
 
+  showLoading () {
+    // wx.showLoading()
+  }
+
+  hideLoading () {
+    // wx.hideLoading()
+  }
+
   messageError (msg) {
-    console.error(msg)
+    // wx.showToast({
+    //   icon: 'none',
+    //   title: msg
+    // })
   }
 }
 
 // .net 接口通用
-export default new RequestCommon(function (data) {
-  if (data.code === 0) {
-    return data.data
-  }
-  return new Error(data.message)
-})
+// export default new RequestCommon(function (data) {
+//   if (data.code === 0) {
+//     return data.data
+//   }
+//   return new Error(data.message)
+// })
 
 // go 接口通用
 // export const ajaxGo = new AjaxComm()
